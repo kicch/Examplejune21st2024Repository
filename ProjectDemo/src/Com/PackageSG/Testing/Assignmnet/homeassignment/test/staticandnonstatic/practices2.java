b4b157c0b3e3f0d089f404aed7947ad1c20f93c2
VenuGopal <venugopalkallur@gmail.com>
2024-08-14 15:41:48 +0530

    sample2

--- ProjectDemo/src/Com/PackageSG/Testing/Assignmnet/homeassignment/test/staticandnonstatic/practices2.java
+++
@@ -1,9 +0,0 @@
-package Com.PackageSG.Testing.Assignmnet.homeassignment.test.staticandnonstatic;
-class Student {
-
-}
-public class practices2 {
-    public static void main(String[] args) {
-
-    }
-}
